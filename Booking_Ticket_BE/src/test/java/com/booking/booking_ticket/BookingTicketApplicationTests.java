01cae2f60a5f3ed5a5018a0b04f096d405b4bb3f
tuanduog <tuanduog04@gmail.com>
2025-07-17 14:53:13 +0700

    init

---
+++ Booking_Ticket_BE/src/test/java/com/booking/booking_ticket/BookingTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.booking_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Booking_Ticket_FE/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (  
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
